7a9bba6b53ce3112d8a47c75d3f8bca4d231989e
GuiGasp1477 <luffy272727@gmail.com>
2022-09-23 17:32:32 +0200

    Page de prestations

--- Site_Mr_Eycs/src/Controller/SiteBoulangController.php
+++ Site_Mr_Eycs/src/Controller/SiteBoulangController.php
@@ -9,11 +9,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class SiteBoulangController extends AbstractController
 {
     /**
-     * @Route("/", name="app_site_boulang")
+     * @Route("/", name="Index")
      */
     public function index(): Response
     {
         return $this->render('site_boulang/index.html.twig', [
+            'controller_name' => 'SiteBoulangController',
+        ]);
+    }
+
+    /**
+     * @Route("/prestations", name="Prestations")
+     */
+    public function prestations(): Response
+    {
+        return $this->render('site_boulang/prestations.html.twig', [
+            'controller_name' => 'SiteBoulangController',
+        ]);
+    }
+
+    /**
+     * @Route("/commandes/produits", name="Produits")
+     */
+    public function produits(): Response
+    {
+        return $this->render('site_boulang/commandes/produits.html.twig', [
+            'controller_name' => 'SiteBoulangController',
+        ]);
+    }
+
+    /**
+     * @Route("/commandes/evenements", name="Evenements")
+     */
+    public function evenements(): Response
+    {
+        return $this->render('site_boulang/commandes/evenements.html.twig', [
             'controller_name' => 'SiteBoulangController',
         ]);
     }
