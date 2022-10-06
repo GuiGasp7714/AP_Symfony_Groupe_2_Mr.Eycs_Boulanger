2fcc96d06705a8b6b84c39d319ed973256e665da
Purvanas <arnaudmirocha@gmail.com>
2022-10-06 14:56:41 +0200

    Feature1.2
    Page de création de compte fini (sauf css)
    page de connexion => en cours

--- Site_Mr_Eycs/src/Controller/SiteBoulangController.php
+++ Site_Mr_Eycs/src/Controller/SiteBoulangController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use App\Form\UtilisateurType;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Request;
 
 class SiteBoulangController extends AbstractController
 {
@@ -20,12 +26,40 @@ class SiteBoulangController extends AbstractController
     }
 
     /**
+     * @Route("/creationDeCompte", name="creationDeCompte")
+     */
+    public function creationDeCompte(Request $request, EntityManagerInterface $manager): Response
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($utilisateur);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_site_boulang');
+        }
+        return $this->render('site_boulang/creationDeCompte.twig', [
+            'controller_name' => 'SiteBoulangController',
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
      * @Route("/connexion", name="connexion")
      */
-    public function Connexion(): Response
+    public function connexion(Request $request, EntityManagerInterface $manager): Response
     {
-        $form = $this->createForm(UtilisateurType::class);
-        return $this->render('site_boulang/Connexion.html.twig', [
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($utilisateur);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_site_boulang');
+        }
+        return $this->render('site_boulang/connexion.html.twig', [
             'controller_name' => 'SiteBoulangController',
             'form' => $form->createView()
         ]);
--- Site_Mr_Eycs/src/Form/UtilisateurType.php
+++ Site_Mr_Eycs/src/Form/UtilisateurType.php
@@ -7,16 +7,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
 class UtilisateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('pseudo')
             ->add('mdp')
-            ->add('Verifmdp')
+            ->add('typeUtilisateur')
             ->add('adresseMail')
             ->add('telephone');
     }
