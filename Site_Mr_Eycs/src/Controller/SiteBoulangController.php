3241ed1237c518cf5bec9f3296dc71769d318ee3
GuiGasp1477 <luffy272727@gmail.com>
2022-10-07 17:30:03 +0200

    Form ajout d'avis

--- Site_Mr_Eycs/src/Controller/SiteBoulangController.php
+++ Site_Mr_Eycs/src/Controller/SiteBoulangController.php
@@ -1,30 +1,83 @@
 <?php
 
+
 namespace App\Controller;
 
+use App\Entity\Avis;
+use App\Form\AvisType;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Request;
 
 class SiteBoulangController extends AbstractController
 {
     /**
      * @Route("/", name="app_site_boulang")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $avis = new Avis();
+
+
+        $formAvis = $this->createForm(AvisType::class, $avis);
+
+        $formAvis->handleRequest($request);
+        if ($formAvis->isSubmitted() && $formAvis->isValid()) {
+            $entityManager->persist($avis);
+            $entityManager->flush();
+            return $this->redirectToRoute('avis');
+        }
         return $this->render('site_boulang/index.html.twig', [
             'controller_name' => 'SiteBoulangController',
+            'formAvis' => $formAvis->createView()
         ]);
     }
 
     /**
      * @Route("/connexion", name="connexion")
      */
-    public function Connexion(): Response
+    public function Connexion(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $avis = new Avis();
+
+        $formAvis = $this->createForm(AvisType::class, $avis);
+        $formAvis->handleRequest($request);
+
+        if ($formAvis->isSubmitted() && $formAvis->isValid()) {
+            $entityManager->persist($avis);
+            $entityManager->flush();
+            return $this->redirectToRoute('avis');
+        }
         return $this->render('site_boulang/Connexion.html.twig', [
             'controller_name' => 'SiteBoulangController',
+            'formAvis' => $formAvis->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/avis", name="avis")
+     */
+    public function Avis(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $avis = new Avis();
+
+        $formAvis = $this->createForm(AvisType::class, $avis);
+
+        $formAvis->handleRequest($request);
+        if ($formAvis->isSubmitted() && $formAvis->isValid()) {
+            $entityManager->persist($avis);
+            
+            $entityManager->flush();
+            return $this->redirectToRoute('avis');
+        }
+        return $this->render('site_boulang/Avis.html.twig', [
+            'controller_name' => 'SiteBoulangController',
+            'formAvis' => $formAvis->createView()
         ]);
     }
 }
