fe36ee8bb5583a9d16e515a8c7f4f060078ca8dc
Purvanas <arnaudmirocha@gmail.com>
2022-10-10 19:01:51 +0200

    Feature 1.1.5 Backup

--- Site_Mr_Eycs/src/Form/RegistrationFormType.php
+++ Site_Mr_Eycs/src/Form/RegistrationFormType.php
@@ -25,11 +25,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Mot de passe obligatoire',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit être d\'au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
